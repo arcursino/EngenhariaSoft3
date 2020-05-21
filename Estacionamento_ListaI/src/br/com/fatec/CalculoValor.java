043c6fc554558b114ac9b45d81f26e8f2dbdd42e
arcursino <ariana.cursino@urbemobile.com.br>
2020-05-21 10:53:26 -0300

    organiza arquivos

--- Estacionamento_ListaI/src/br/com/fatec/CalculoValor.java
+++
@@ -1,6 +0,0 @@
-package br.com.fatec;
-
-public interface CalculoValor {
-	public double valor();
-	double valorConta(double tempo, Veiculo veiculo);
-}
--- LojaVirtual_ListaII/src/br/com/negocio/fatec/Desconto.java
+++
@@ -1,7 +0,0 @@
-package br.com.negocio.fatec;
-
-public interface Desconto {
-	public double valor();
-	
-}
-
